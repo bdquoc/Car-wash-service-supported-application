10d336cd00c201077038186ff81920632b6aeb5d
bdquoc <22520098@gm.uit.edu.vn>
2024-11-14 23:31:21 +0700

    CRUD: delete

--- NODEJS/src/controllers/homeController.js
+++ NODEJS/src/controllers/homeController.js
@@ -28,9 +28,6 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log('---------------------------')
-    console.log(data)
-    console.log('---------------------------')
 
     return res.render('displayCRUD.ejs', {
         dataTable: data
@@ -58,6 +55,17 @@ let putCRUD = async (req, res) => {
         dataTable: allUsers
     })
 }
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete user succeed!')
+    }
+    else {
+        return res.send('User not found!')
+    }
+}
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
@@ -66,5 +74,6 @@ module.exports = {
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
     putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 
 }
