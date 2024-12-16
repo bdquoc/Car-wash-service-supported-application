1659e2a7582763aa815a3a9754bfe589fe8b2ea5
bdquoc <22520098@gm.uit.edu.vn>
2024-12-16 23:23:15 +0700

    DELETE user

--- REACTJS/src/containers/System/UserManage.js
+++ REACTJS/src/containers/System/UserManage.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
-import { getAllUsers, createNewUserService } from '../../services/userService';
+import { getAllUsers, createNewUserService, deleteUserSerice } from '../../services/userService';
 import ModalUser from './ModalUser';
+import { emitter } from '../../utils/emitter';
 
 class UserManage extends Component {
 
@@ -50,6 +51,22 @@ class UserManage extends Component {
                 this.setState({
                     isOpenModalUser: false
                 })
+                emitter.emit('EVENT_CLEAR_MODAL_DATA', { 'id': 'your id' })
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    handleDeleteUser = async (user) => {
+        console.log('Delete', user)
+        try {
+            let res = await deleteUserSerice(user.id);
+            console.log(res)
+            if (res && res.errCode === 0) {
+                await this.getAllUsersFromReact();
+            } else {
+                alert(res.errMessage)
             }
         } catch (e) {
             console.log(e);
@@ -93,7 +110,7 @@ class UserManage extends Component {
                                         <td>{item.address}</td>
                                         <td>
                                             <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
-                                            <button className="btn-delete"><i className="fas fa-trash"></i></button>
+                                            <button className="btn-delete" onClick={() => this.handleDeleteUser(item)}><i className="fas fa-trash"></i></button>
                                         </td>
                                     </tr>
                                 )
