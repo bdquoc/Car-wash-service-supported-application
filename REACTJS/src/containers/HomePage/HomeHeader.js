f8e8b4bddf1c80b7ce7494bf41370e4a887a826a
bdquoc <22520098@gm.uit.edu.vn>
2024-12-17 03:31:50 +0700

    update home

--- REACTJS/src/containers/HomePage/HomeHeader.js
+++ REACTJS/src/containers/HomePage/HomeHeader.js
@@ -6,32 +6,68 @@ class HomeHeader extends Component {
 
     render() {
         return (
-            <div className="home-header-container">
-                <div className="home-header-content">
-                    <div className="left-content">
-                        <i class="fas fa-bars"></i>
-                        <div className="header-logo"></div>
-                    </div>
-                    <div className="center-content">
-                        <div className="child-content">
-                            <div><b>Dịch vụ</b></div>
+            <React.Fragment>
+                <div className="home-header-container">
+                    <div className="home-header-content">
+                        <div className="left-content">
+                            <i class="fas fa-bars"></i>
+                            <div className="header-logo"></div>
                         </div>
-                        <div className="child-content">
-                            <div><b>Sản phẩm</b></div>
+                        <div className="center-content">
+                            <div className="child-content">
+                                <div><b>Dịch vụ</b></div>
+                            </div>
+                            <div className="child-content">
+                                <div><b>Sản phẩm</b></div>
+                            </div>
+                            <div className="child-content">
+                                <div><b>Hệ thống</b></div>
+                            </div>
+                            <div className="child-content">
+                                <div><b>Blog</b></div>
+                            </div>
                         </div>
-                        <div className="child-content">
-                            <div><b>Hệ thống</b></div>
+                        <div className="right-content">
+                            <div className="support"><i className="fas fa-question-circle"></i>Hỗ trợ</div>
+                            <div className="flag">VN</div>
                         </div>
-                        <div className="child-content">
-                            <div><b>Blog</b></div>
-                        </div>
-                    </div>
-                    <div className="right-content">
-                        <div className="support"><i className="fas fa-question-circle"></i>Hỗ trợ</div>
-                        <div className="flag">VN</div>
                     </div>
                 </div>
-            </div>
+                <div className="home-header-banner">
+                    <div className="content-up">
+                        <div className="title1">BOOKING CAR WASH SERVICE</div>
+                        <div className="title2"><b>BẢO DƯỠNG CHĂM SÓC XE MỘT CÁCH TOÀN DIỆN</b></div>
+                        <div className="search">
+                            <i className="fas fa-search"></i>
+                            <input type="text" placeholder="Tìm dịch vụ, thợ rửa xe" />
+                        </div>
+                    </div>
+                    <div className="content-down">
+                        <div className="options">
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fas fa-car"></i></div>
+                                <div className="text-child">Detailing - Chăm sóc tại nhà</div>
+                            </div>
+                            <div className="option-child">
+                                <div className="icon-child"><i class="fas fa-cart-plus"></i></div>
+                                <div className="text-child">Sản phẩm và phụ kiện </div>
+                            </div>
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fas fa-cogs"></i></div>
+                                <div className="text-child">Bảo dưỡng phanh, thắng đĩa</div>
+                            </div>
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fas fa-wrench"></i></div>
+                                <div className="text-child">Bảo dưỡng, thay lốp</div>
+                            </div>
+                            <div className="option-child">
+                                <div className="icon-child"><i className="fab fa-searchengin"></i></div>
+                                <div className="text-child">Bảo dưỡng khoang động cơ</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         );
     }
 
