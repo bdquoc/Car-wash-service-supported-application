116c3350565db767e20744d25c8a84ec625777b3
NHBach-22520091 <22520091@gm.uit.edu.vn>
2024-12-21 00:30:37 +0700

    add api allcode

--- NODEJS/src/controllers/userController.js
+++ NODEJS/src/controllers/userController.js
@@ -1,3 +1,4 @@
+import { json } from 'body-parser';
 import userService from '../services/userService';
 
 let handleLoging = async (req, res) => {
@@ -62,10 +63,24 @@ let handleEditUser = async (req, res) => {
     return res.status(200).json(message);
 }
 
+let getAllCode = async (req, res) => {
+    try{
+        let data = await userService.getAllCodeService(req.query.type);
+        return res.status(200).json(data);
+    } catch (e) {
+        console.log('Get all code error: ',e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+
 module.exports = {
     handleLoging: handleLoging,
     handleGetAllUsers: handleGetAllUsers,
     handleCreateNewUser: handleCreateNewUser,
     handleEditUser: handleEditUser,
-    handleDeleteUser: handleDeleteUser
+    handleDeleteUser: handleDeleteUser,
+    getAllCode: getAllCode
 }
