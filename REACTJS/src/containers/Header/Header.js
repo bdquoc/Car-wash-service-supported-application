ad8518040adfe784328701d2bed50eec4dbb123f
NHBach-22520091 <22520091@gm.uit.edu.vn>
2024-12-21 11:58:12 +0700

    update api login, devide admin menu, add more menus, update file rootReducer

--- REACTJS/src/containers/Header/Header.js
+++ REACTJS/src/containers/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { FormattedMessage } from 'react-intl';
 import * as actions from "../../store/actions";
 import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
@@ -13,8 +13,8 @@ class Header extends Component {
         this.props.changeLanguageAppRedux(language)
     }
     render() {
-        const { processLogout, language } = this.props;
-
+        const { processLogout, language, userInfo } = this.props;
+        console.log('check usser info', userInfo);
         return (
             <div className="header-container">
                 {/* thanh navigator */}
@@ -23,6 +23,9 @@ class Header extends Component {
                 </div>
                 
                 <div className='languages'>
+                    <span className='welcome'>
+                        <FormattedMessage id="homeheader.welcome"/> 
+                        {userInfo && userInfo.firstName ? userInfo.firstName : ''}</span>
                     <span className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'} 
                     onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}>
                         VN
@@ -49,6 +52,7 @@ const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
         language: state.app.language,
+        userInfo: state.user.userInfo,
     };
 };
 
--- REACTJS/src/containers/Header/menuApp.js
+++ REACTJS/src/containers/Header/menuApp.js
@@ -1,14 +1,59 @@
 export const adminMenu = [
-    { //hệ thống
-        name: 'menu.system.header', menus: [
+
+    { //quản lí người dùng
+        name: 'menu.admin.manage-user', 
+        menus: [
             {
-                name: 'menu.system.system-administrator.header',
-                subMenus: [
-                    { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
-                    { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                ]
+                name: 'menu.admin.crud', link: '/system/user-manage'
             },
-            // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
+
+            {
+                name: 'menu.admin.crud-redux', link: '/system/user-redux'
+            },
+
+            {
+                 name: 'menu.admin.manage-employee', link: '/system/user-employee'
+                // subMenus: [
+                //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
+                //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
+                // ]
+            },
+
+            {
+                name: 'menu.admin.manage-admin', link: '/system/user-admin'
+            },
+
+            
+        ]
+    },
+
+    { //quản lí cơ sở
+        name: 'menu.admin.facility', 
+        menus: [
+            {
+                name: 'menu.admin.manage-facility', link: '/system/manage-facility'
+            },
+
+        ]
+    },
+
+    { //quản lí dịch vụ
+        name: 'menu.admin.specialty', 
+        menus: [
+            {
+                name: 'menu.admin.manage-specialty', link: '/system/manage-specialty'
+            },
+
+        ]
+    },
+
+    { //quản lí tips
+        name: 'menu.admin.tips', 
+        menus: [
+            {
+                name: 'menu.admin.manage-tips', link: '/system/manage-tips'
+            },
+
         ]
     },
 ];
